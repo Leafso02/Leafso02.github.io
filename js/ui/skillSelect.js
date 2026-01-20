119844136ad198262038cfbc3c777467872a14c3
Leafso02 <Leaf.fortnite@gmail.com>
2026-01-21 01:55:39 +0900

    155

--- js/ui/skillSelect.js
+++ js/ui/skillSelect.js
@@ -118,7 +118,7 @@ function adjustSkillLevelInput(multipliers, skillType, levelInput) {
   levelInput.step = 1;
 
   let targetLv = DEFAULT_SKILL_LEVEL[skillType] ?? min;
-  console.log(targetLv)
+  console.log(DEFAULT_SKILL_LEVEL[skillType])
 
   if (!validLevels.includes(targetLv)) {
     targetLv = findNearestLevel(validLevels, targetLv);
